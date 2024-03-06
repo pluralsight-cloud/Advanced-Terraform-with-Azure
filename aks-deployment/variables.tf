0c8b23b663fcd55c00097b9d16b9b4d2b32e417c
ned1313 <ned.bellavance@gmail.com>
2024-03-06 07:01:50 -0500

    aks

---
+++ aks-deployment/variables.tf
@@ -0,0 +1,28 @@
+# Declare the location variable
+variable "location" {
+   description = "The location where resources should be created"
+   type        = string
+   default     = "East US"
+}
+
+# Declare the prefix variable
+variable "prefix" {
+   description = "The prefix which should be used for all resources"
+   type        = string
+   default     = "tacowagon"
+}
+
+variable "common_tags" {
+  description = "Map of tags to apply to all resources."
+  type        = map(string)
+}
+
+variable "vnet_address_space" {
+  description = "List of address spaces to use for the VNET."
+  type        = list(string)
+}
+
+variable "subnet_configuration" {
+  description = "Map of subnets to create in the VNET. Key is subnet name, value is address spaces."
+  type        = map(string)
+}
